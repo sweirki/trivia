006924a805353dbd4d83953b83369a07adc9a233
Yaseen <michuae@yahoo.com>
2025-09-26 16:44:49 +0300

    Add question screen

---
+++ lib/screens/question_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Question')), 
+      body: Center(
+        child: Text('Question Screen'),
+      ),
+    );
+  }
+}
