24ae0847101d72aa5fc5eadb9601a55bff8ce4bc
Yaseen <michuae@yahoo.com>
2025-09-26 16:45:05 +0300

    Add leaderboard screen

---
+++ lib/screens/leaderboard_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Leaderboard')),
+      body: Center(
+        child: Text('Leaderboard Screen'),
+      ),
+    );
+  }
+}
